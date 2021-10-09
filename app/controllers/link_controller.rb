f0f9c1e94017ff9a7005e4f8050f93d5386a3a27
pawan1498 <pawan.sh.7798@gmail.com>
2021-10-09 12:09:26 +0530

    add basic flow of tagging

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -1,4 +1,45 @@
 class LinkController < ApplicationController
+
     def index
+        @links = Link.all
     end 
+
+    def new 
+        @link = Link.new
+        @tag = Tag.new
+    end 
+    
+    def create
+        @link = Link.new(link_params)
+        @tag = Tag.new(tag_params)
+        @link.save 
+        @tag.save
+        @link.tags << @tag
+        redirect_to show_path
+    end
+    
+    def show 
+    end 
+
+    def search 
+       
+    end
+
+    def result
+        @t = Tag.find_by_tag_name(result_param)
+        @l = @t.links
+    end 
+
+ private 
+    def link_params
+        params.require(:link).permit(:url)
+    end
+
+    def tag_params
+        params.require(:tag).permit(:tag_name)
+    end
+
+    def result_param
+        params.require(:query)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/index', to: "link#index"
+  get '/home', to: "link#index"
+  get '/new', to: "link#new"
+  post '/links',to:"link#create"
+  get '/show' ,to: "link#show"
+  get '/search', to:"link#search"
+  get 'result',to:"link#result"
 end
