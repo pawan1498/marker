3a21683b9114286ecc169ab95a528c54f47541f3
pawan1498 <pawan.sh.7798@gmail.com>
2021-10-27 11:58:46 +0530

    home path change to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/home', to: "link#index"
+  root to: "link#index"
   get '/new', to: "link#new"
   post '/links',to:"link#create"
   get '/show' ,to: "link#show"
