572835bc72dbd752fdc473c3130fd816089ce4a9
pawan1498 <pawan.sh.7798@gmail.com>
2021-12-02 13:30:16 +0530

    basic work flow success

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -1,47 +1,50 @@
 class LinkController < ApplicationController
+  def index
+    @links = Link.all
+  end
 
-    def index
-        @links = Link.all
-    end 
+  def new
+    @group = Group.new
+    @str = @group.group_name()
+  end
 
-    def new 
-        @link = Link.new
-    end 
-    
-    def create
-        group_name = params[:link][:group_id]
-        @group = Group.new(name: group_name)
-        @group.save
-        params[:link][:group_id] = @group.id
-        
+  def create
+    group_name = params[:group_name]
+    @group = Group.new(name: group_name)
+    @group.save
 
-        @link = Link.new(link_params)
-        @link.save
-        redirect_to show_path
+    url = params[:url]
+    tag = params[:tag_list]
+
+    @link = Link.new(group_id: @group.id, url: url, tag_list: tag)
+
+    if @link.save
+      redirect_to show_path
+    else
+      @group.delete
+      render :new
     end
-    
-    def show 
-    end 
+  end
 
-    def search 
-    end
+  def show
+  end
 
-    def result
-        tag = Array.new(10)
-        tag = result_param.split(',')
-        @l = Link.tagged_with(tag[0],  :any => true)
-        tag.each { |x|
-            next if x == tag[0]
-            @l = @l.tagged_with(x, :any => true)
-        }
-    end 
+  def search
+  end
 
- private 
-    def link_params
-        params.require(:link).permit(:group_id,:url,:tag_list)
-    end
+  def result
+    tag = Array.new(10)
+    tag = result_param.split(",")
+    @l = Link.tagged_with(tag[0], :any => true)
+    tag.each { |x|
+      next if x == tag[0]
+      @l = @l.tagged_with(x, :any => true)
+    }
+  end
 
-    def result_param
-        params.require(:query)
-    end
+  private
+
+  def result_param
+    params.require(:query)
+  end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
-    # has_and_belongs_to_many :tags
-    acts_as_taggable_on :tags
-    belongs_to :group
+  # has_and_belongs_to_many :tags
+  acts_as_taggable_on :tags
+  belongs_to :group
 end
