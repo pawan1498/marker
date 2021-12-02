ac66acee70a5b58453a0b376e899ab14d2333b38
pawan1498 <pawan.sh.7798@gmail.com>
2021-12-02 18:11:41 +0530

    add  uniqueness validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,10 @@
 class Group < ApplicationRecord
-  has_many :links
+  has_many :links, dependent: :destroy
+  validates :name ,  uniqueness: true
 
   def group_name()
     name = ["This Group is A+", "The Collective", "Life of Pi", "The Brain Cells", "The Circle", "Image Skeletons", "The Spin Doodles", "Dread Skin", " Geek Syndrome"]
     length = name.size
-    return name[rand(0..length - 1)]
+    return name[rand(0..(length - 1))]
   end
 end
