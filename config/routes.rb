3d747517f9e45727df9ec3ed40688578cf955d30
pawan1498 <pawan.sh.7798@gmail.com>
2022-10-03 00:19:39 +0530

    fix form

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     root to: "users/sessions#new"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root to: "link#index"
   # resources :link
   get '/new', to: "link#new"
   post '/links',to:"link#create"
