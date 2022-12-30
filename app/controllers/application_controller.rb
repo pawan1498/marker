a19a9fb927fd5c5cf064266057f2baf2ff11ceaa
pawan1498 <pawan.sh.7798@gmail.com>
2022-12-30 23:52:56 +0530

    function name change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
+  def after_admin_sign_in_path_for(resource)
     admin_dashboard_index_path
   end
 end
