18f38e0e04bda27e55e87c8d568b954e4d3f1e47
pawan1498 <pawan.sh.7798@gmail.com>
2023-01-27 21:31:20 +0530

    update

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -1,62 +1,86 @@
 class LinkController < ApplicationController
-	before_action :authenticate_user!, only: %i[ new show search index]
-	def index
-		@links = current_user.links
-		@tags=[]
-		@links.each do |link|
-			link.tags.each do |tag |
-				@tags << tag
-			end 
-		end 
-	end 
+  before_action :authenticate_user!, only: %i[ new show search index ]
 
-	def new
-		@link = Link.new
-	end
-	
-	def create
-		@link = Link.new(url: link_params[:url], user_id: current_user.id)
-		owned_tag_list = link_params[:tag_list]
-		current_user.tag(@link, :with =>(owned_tag_list), :on => :tags, :skip_save => true)
-		@link.save
-		redirect_to root_path
-	end
-	
-	def show
-	end
+  def index
+    @links = current_user.links
+    @tags = []
+    @links.each do |link|
+      link.tags.each do |tag|
+        @tags << tag
+      end
+    end
+  end
 
-	def search
-		@tag = ActsAsTaggableOn::Tag.all
-		@tags = @tag.collect(&:name)
-	end
+  def new
+    @link = Link.new
+  end
 
-	def result
-		tag = []
-		tag = result_param.split(',')
-		@l = current_user.links.tagged_with(tag,  :any => true)
-		# tag.each { |x|
-		# 		@l = @l.tagged_with(x, :any => true)
-		# }
-	end
+  def create
+    @link = Link.new(url: link_params[:url], user_id: current_user.id)
+    owned_tag_list = link_params[:tag_list]
+    current_user.tag(@link, :with => (owned_tag_list), :on => :tags, :skip_save => true)
+    @link.save
+    redirect_to root_path
+  end
 
-	def autocomplete
-		keyword = params[:text]
-		@all_tags = current_user.owned_tags
-		if keyword.present? 
-			@all_tags = @all_tags.where("name LIKE ? OR name LIKE ?", "#{keyword}%", "% #{keyword}%")
-		else
-			@all_tags = nil
-		end 
-		render json: @all_tags.present? ? @all_tags.pluck(:name) : nil
-	end
+  def show
+    @link = Link.find(params[:id])
+  end
 
-private
+  def edit
+    @link = Link.find(params[:id])
+  end
 
-	def link_params
-		params.require(:link).permit(:url,:tag_list)
-	end
+  # def update
+  # end
 
-	def result_param
-		params.require(:query)
-	end
+  def update
+    #@user = User.find(params[:id])
+    if @link.update(user_params)
+      redirect_to @link
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    redirect_to link_index_path
+  end
+
+  def search
+    @tag = ActsAsTaggableOn::Tag.all
+    @tags = @tag.collect(&:name)
+  end
+
+  def result
+    tag = []
+    tag = result_param.split(",")
+    @l = current_user.links.tagged_with(tag, :any => true)
+    # tag.each { |x|
+    # 		@l = @l.tagged_with(x, :any => true)
+    # }
+  end
+
+  def autocomplete
+    keyword = params[:text]
+    @all_tags = current_user.owned_tags
+    if keyword.present?
+      @all_tags = @all_tags.where("name LIKE ? OR name LIKE ?", "#{keyword}%", "% #{keyword}%")
+    else
+      @all_tags = nil
+    end
+    render json: @all_tags.present? ? @all_tags.pluck(:name) : nil
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :tag_list)
+  end
+
+  def result_param
+    params.require(:query)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
       }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "link#index"
-  get '/new', to: "link#new"
-  post '/links',to:"link#create"
-  get '/show' ,to: "link#show"
+  # get '/new', to: "link#new"
+  # post '/links',to:"link#create"
+  # get '/show' ,to: "link#show"
   get '/search', to:"link#search"
   get 'result',to:"link#result"
   get '/autocomplete', to:"link#autocomplete"
-
+  resources :link
   namespace :admin do
     resources :dashboard
   end
